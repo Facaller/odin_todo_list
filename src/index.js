ad75bc9940a84d0f2ff9ce4acebecca9ac77a892
Facaller <ashwin.metal13@gmail.com>
2024-12-02 15:02:29 +0200

    Test methods, realise I deleted validation method like an ape and broke it

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import "./styles.css"
-import Todo from "./todos.js";
+import { Todo, TodoList } from "./todos.js";
 import Project from "./projects.js";
-
-const test = new Todo("Work", "Hard", 1010, 1);
-console.log(test);
 
 const project = new Project("Build website", "Create a personal portfolio", "2024-12-10");
 const todo = new Todo ("Buy groceries", "Milk, eggs, bread", "2024-12-01");
+const todolist = new TodoList()
+
+todolist.addTask(todo);
+todolist.updateTask(todo.id, "Buy burgers", "Cheese", "2010-10-10")
+
 console.log(todo.id);
 console.log(project.id);
+console.log(todo);
--- src/todos.js
+++ src/todos.js
@@ -1,6 +1,6 @@
 import { Task, TaskList } from "./task";
 
-class Todo extends Task {
+export class Todo extends Task {
     constructor (title, details, date) {
         super (title, details);
         this.date = date;
@@ -8,11 +8,33 @@ class Todo extends Task {
     }
 }
 
-class TodoList extends TaskList {
+export class TodoList extends TaskList {
     constructor () {
         super();
         this.todos = [];
     }
-}
 
-export default Todo;
+    updateTask (id, newTitle, newDetails, newDate) {
+        super.updateTask(id, newTitle, newDetails)
+
+        const task = this.tasks.find(task => task.id === id);
+
+        if (!task) {
+            console.log('Task not found');
+            return;
+        }
+
+        let updated = false;
+
+        if (newDate && this.validate(newDate)) {
+        task.date = newDate
+        updated = true;
+        }
+
+        if (!updated) {
+            console.log('No valid changes');
+        }
+    }
+
+    
+}
