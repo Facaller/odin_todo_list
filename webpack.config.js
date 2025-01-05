6f3adb95f084ec282415ca2be0f83be9b98e9a3e
Facaller <ashwin.metal13@gmail.com>
2025-01-05 19:36:10 +0200

    Add font files, add fonts to config

--- webpack.config.js
+++ webpack.config.js
@@ -20,18 +20,25 @@ module.exports = {
     ],
     module: {
         rules: [
-        {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-        },
-        {
-            test: /\.html$/i,
-            loader: "html-loader",
-        },
-        {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: "asset/resource",
-        },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(woff|woff2)$/i,
+                type: "asset/resource",
+                generator: {
+                    filename: "assets/fonts/[name][ext][query]",
+                },
+            },
         ],
     },
 };
