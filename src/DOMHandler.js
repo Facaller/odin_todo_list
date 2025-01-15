9013f74dc779c187edd5f9c3358a0b351922013c
Facaller <ashwin.metal13@gmail.com>
2025-01-15 17:15:16 +0200

    Add sidebar event listener, remove dummy HTML in sidebar

--- src/DOMHandler.js
+++ src/DOMHandler.js
@@ -1,5 +1,37 @@
 class DOMElements {
     constructor () {
-        
+        this.sidebarNav = document.getElementById('sidebarNav');
+        this.projects   = document.querySelector('.project-container')
+    }
+}
+
+class DOMHandler {
+    constructor (app) {
+        this.app = app;
+        this.elements = new DOMElements();
+    }
+
+    sidebarEvents () {
+        this.elements.sidebarNav.addEventListener('click', (event) => {
+            if (event.target.id === 'allTasks') {
+                getAllTasks();
+            } else if (event.target.id === 'today') {
+                getTasksDueToday();
+            } else if (event.target.id === 'next7Days') {
+                getTasksForNextSevenDays();
+            } else if (event.target.id === 'Important') {
+                getAllImportantTasks();
+            } else if (event.target.id === 'addProject') {
+                addTask(task);
+            }
+        });
+    }
+
+    projectEvents () {
+        this.elements.projects.addEventListener('click', (event) => {
+            if () {
+                
+            }
+        })
     }
 }
